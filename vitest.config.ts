643b6d8eadc13bafa45633a9719caa8b7da49c13
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-03-20 22:18:22 +0100

    dont seem to need eslint ignore here

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     react({
       exclude: /\.stories\.(t|j)sx?$/,
       include: "**/*.tsx",
